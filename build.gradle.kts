84ea542bafe276c86758e7e6740e10a92a605783
Vincent Carluer <vcarluer@gmail.com>
2021-05-25 23:45:24 +0200

    oauth2 spring boot security

--- build.gradle.kts
+++ build.gradle.kts
@@ -120,6 +120,12 @@ subprojects {
     //  included dynamically if the 'platform' build property is 'azure'
     implementation("com.azure.spring:azure-spring-boot-starter-cosmos:3.5.0")
     implementation("com.azure.spring:azure-spring-boot-starter-storage:3.5.0")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:2.4.5")
+    implementation("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
+
+
+
 
     testImplementation(kotlin("test"))
     testImplementation("io.mockk:mockk:1.11.0")
